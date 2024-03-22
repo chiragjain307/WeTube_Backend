c2b934594baa1d531e52cf6c6c8e3d4826118b10
chiragjain307 <chiragjain.jain7@gmail.com>
2024-03-22 19:17:23 +0530

    updates

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,13 @@
+import {v2 as cloudinary} from 'cloudinary' //file storage service
+import fs from 'fs' // Node.js file system module
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECERT 
+});
+
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
